328aaad3f67d4288e4d1752c44005aa24d4b2b86
Anders Albert <anders.albert@arundo.com>
2021-03-17 07:52:40 +0100

    feat: Sign-up component implemented

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-import {auth} from "./firebase/firebase.utils"
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils"
 
 class App extends React.Component {
     constructor(props) {
@@ -14,12 +14,26 @@ class App extends React.Component {
             currentUser: null
         }
     }
+
     unsubscribeFromAuth = null
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-                this.setState({currentUser: user});
-                console.log(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+                // this.setState({currentUser: user});
+                if (userAuth) {
+                    const userRef = await createUserProfileDocument(userAuth);
+                    userRef.onSnapshot(snapshot => {
+                        this.setState({
+                            currentUser: {
+                                id: snapshot.id,
+                                ...snapshot.data()
+                            }
+                        })
+                    })
+                } else {
+                    this.setState({currentUser: userAuth})
+                }
+
             }
         )
     }
@@ -31,7 +45,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser} />
+                <Header currentUser={this.state.currentUser}/>
                 <Switch>
                     <Route exact={true} path="/" component={HomePage}/>
                     <Route path="/shop" component={ShopPage}/>
