b3f73a45ed64ae7a3721be57652bec402c00859a
Anders Albert <anders.albert@arundo.com>
2021-03-23 20:20:29 +0100

    feat: Added checkout items

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.components";
 import Header from "./components/header/header.component";
 import {auth, createUserProfileDocument} from "./firebase/firebase.utils"
 import {setCurrentUser} from "./redux/user/user.actions";
@@ -46,6 +47,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact={true} path="/" component={HomePage}/>
                     <Route path="/shop" component={ShopPage}/>
+                    <Route exact={true} path="/checkout" component={CheckoutPage}/>
                     <Route exact={true} path="/signin"
                            render={() => this.props.currentUser ? (<Redirect to="/"/>) : (<SignInAndSignUpPage/>)}/>
                 </Switch>
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -18,3 +18,11 @@ export const selectCartItemsCount = createSelector(
         (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
         0)
 )
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulatedCost, cartItem) => accumulatedCost + cartItem.quantity * cartItem.price,
+        0
+    )
+)
