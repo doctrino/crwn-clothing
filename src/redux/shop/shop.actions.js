9dfb40a85d95347c7878831ade0acf31b42f35be
Anders Albert <anders.albert@arundo.com>
2021-03-27 15:42:05 +0100

    refactor: Moved async fetching of shop data into redux

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,27 @@
 import ShopActionTypes from "./shop.types";
+import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = (collectionsMap) => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START,
+})
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap
 })
+
+export const fetchCollectionsFailure = error => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: error
+})
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection("collections");
+        dispatch(fetchCollectionsStart());
+        collectionRef.get().then(snapshot => {
+            const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionMap));
+        }).catch(error => dispatch(fetchCollectionsFailure(error)))
+    }
+}
